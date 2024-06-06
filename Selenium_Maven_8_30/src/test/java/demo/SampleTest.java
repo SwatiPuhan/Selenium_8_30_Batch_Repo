56299ae5f95c4dec9fbdb8380ae40058c405f778
SwatiPuhan <164142424+SwatiPuhan@users.noreply.github.com>
2024-06-06 10:30:24 +0530

    Update SampleTest.java

--- Selenium_Maven_8_30/src/test/java/demo/SampleTest.java
+++ Selenium_Maven_8_30/src/test/java/demo/SampleTest.java
@@ -1,35 +1,21 @@
-//package demo;
-//
-//import org.testng.Reporter;
-//
-//public class SampleTest
-//{
-//	public void test()
-//	{
-//		Reporter.log("SampleTest  class is Executed",true); //because it is  pre-fixed with test");
-                 	Reporter.log("Sampletest class is modified",true); 
-	Reporter.log("Sampletest class is modified again",true); 
-//	}
-//		
-//	
-//}
-//
-
-
 package demo;
 
 import org.testng.Reporter;
-import org.testng.annotations.Test;
 
-public class SampleTest {
-    
-    @Test
-    public void test() {
-        //System.out.println("SampleTest class is Executed");
-    	Reporter.log("Sampletest class is Executed",true); 
-	    	Reporter.log("Sampletest class is modified",true); 
-	                	Reporter.log("Sampletest class is modified again and again",true); 
+public class SampleTest
+{
+	public void test()
+	{
+		Reporter.log("SampleTest  class is Executed",true); //because it is  pre-fixed with test");
+                 	Reporter.log("Sampletest class is modified",true); 
+	Reporter.log("Sampletest class is modified again",true); 
+		Reporter.log("Sampletest class is modified again and again",true); 
 	Reporter.log("Sampletest class is modified again swati",true);//because it is not pre-fixed with test");
+	}
+		
 	
-    }
 }
+
+
+
+
